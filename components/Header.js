e093080b51a70f6de0a4af2a70e49549cbe26b26
ismam3 <ismamlabib3@gmail.com>
2021-09-11 23:21:14 +0600

    Landing div is designed

---
+++ components/Header.js
@@ -0,0 +1,6 @@
+import styles from "../styles/Home.module.css"
+export default function Header(){
+    return <div className={styles.landing_div}>
+        <h1>Welcome to my photography portfolio</h1>
+    </div>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <head>
         <title>Fake photography</title>
       </head>
+      <Header></Header>
     </div>
   )
 }
