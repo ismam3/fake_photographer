458e2258def39b2e1fa7f360e258a168593cfb86
ismam3 <ismamlabib3@gmail.com>
2021-09-11 22:22:22 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+### A photography portfolio for Mr.Funky
+
+## Next is used in frontend
+
+Live link: (https://ismam3.github.io/fake_photographer/)[https://ismam3.github.io/fake_photographer/]
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Fake photography</title>
+      </head>
+    </div>
+  )
+}
